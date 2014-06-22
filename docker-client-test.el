fea5de59e3dd621baafb6d9e64af5720ecfa81eb
Pith <pierre.thirouin@gmail.com>
2014-06-22 02:44:29 +0200

    Added licence in all files

--- docker-client-test.el
+++ docker-client-test.el
@@ -1,8 +1,29 @@
-;; This file test docker-client.el.
-;;
+;;; docker-client-test.el --- Tests docker-client.el
+
+;; URL: https://github.com/pith/docker-client
+;; Created: 16th June 2014
+;; Version: 0.1
+
+;; Copyright (C) 2014 Pierre THIROUIN <pierre.thirouin@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Description:
+
 ;; Uncomment the following line and the set one of your running container.
 ;; (defvar container-name "")
-;;
+
 ;; Then eval these lines one by one.
 
 (require 'docker-client)
--- docker-client.el
+++ docker-client.el
@@ -1,20 +1,39 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;                        Docker Client
-;;
+;;; docker-client.el --- Docker Client for emacs
+
+;; URL: https://github.com/pith/docker-client
+;; Created: 16th June 2014
+;; Version: 0.1
+
+;; Copyright (C) 2014 Pierre THIROUIN <pierre.thirouin@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Description:
+
 ;; This file provides emacs lisp functions to reach the docker REST API. There is
 ;; no interactive functions. These functions are intend to be used in UI modes (eg. 
 ;; helm-docker).
-;;
+
 ;; /!\ This is still a work in progress don't expect it to work.
-;;
-;; Configuration:
-;;
+
+;;; Configuration:
+
 ;; To use this file you have to define the host to request and the container to work with.
-;;
+
 ;; (defvar docker-host "myhost.com")
 ;; (defvar docker-port "8080")
-;; 
+
 
 (require 'url)
 (require 'json)
